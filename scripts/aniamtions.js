df09884053a17429811f913b9293c94a657c79f7
kennedfer <kenned14santos@gmail.com>
2024-04-13 14:01:13 -0300

    feat: Adicionando o codigo base para animacoes de scroll

---
+++ scripts/aniamtions.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+  console.log("aiaia");
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-animation");
+    } else {
+      entry.target.classList.remove("show-animation");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".animate");
+hiddenElements.forEach((el) => observer.observe(el));
